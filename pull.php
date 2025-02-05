1a366d702a0d4293676064fd3a99240bc4563a77
Ashishtharu <129973502+ashishtharu1209@users.noreply.github.com>
2025-02-05 13:55:20 +0545

    Update pull.php

--- pull.php
+++ pull.php
@@ -1,4 +1,4 @@
 <?php
-$output = shell_exec('git pull https://github.com/ashishtharu1209/AutoPull1.git main');
+$output = shell_exec('git pull https://github.com/ashishtharu1209/AutoPull1.git master');
 echo "<pre>$output</pre>";
 ?>
